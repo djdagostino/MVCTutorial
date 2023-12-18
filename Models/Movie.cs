adb93fcca4b9fb460ac184c8e5889cad11486c60
djdagostino <152912672+daviddagWGELD@users.noreply.github.com>
2023-12-18 13:51:48 -0500

    Modified to Part 8, msft tutorial

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,4 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
 
 namespace MvcMovie.Models;
 
@@ -6,8 +10,21 @@ public class Movie
 {
     public int Id { get; set; }
     public string? Title { get; set; }
+
+    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
+    [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
+    public string? Rating { get; set; }
+}
+
+public class MovieGenreViewModel
+{
+    public List<Movie>? Movies { get; set; }
+    public SelectList? Genres { get; set; }
+    public string? MovieGenre { get; set; }
+    public string? SearchString { get; set; }
+
 }
